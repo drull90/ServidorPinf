809926a9dbdfc12dfaa53441545741114746d4a1
angelgaviraasencio <angelgavira.ag@gmail.com>
2021-01-17 11:20:11 +0100

    .

--- src/cliente/src/app/components/bet/bet.component.ts
+++ src/cliente/src/app/components/bet/bet.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
+import { AuthenticationService } from '../service/authentication.service';
+
+
 
 @Component({
   selector: 'app-bet',
@@ -9,17 +14,26 @@ import { environment } from 'src/environments/environment';
 export class BetComponent implements OnInit {
 
   token: string = "";
+  apuestas: any = [];
 
 
-  constructor() { }
-
+  constructor(public auth: AuthenticationService, private router: Router, private httpClient: HttpClient) { }
+  
   async ngOnInit() {
-    let user= await this.auth.getCurrentUser();
-    let token = await user?.getIdToken();
-    let tokenString = "Bearer " + token;
-    this.token = tokenString;
+    
+      let user= await this.auth.getCurrentUser();
+      let token = await user?.getIdToken();
+      let tokenString = "Bearer " + token;
+      this.token = tokenString;
 
-    this.httpClient.get(environment.url + "/apuestas", )
-  }
+      this.httpClient.get(environment.url + "/apuestas", {headers: {'Authorization': tokenString}})
+      .subscribe(
+        (response: any) => {
+          this.apuestas = response.data;
+
+          console.log(this.apuestas);
+        }
+      );
+    }
 
 }
